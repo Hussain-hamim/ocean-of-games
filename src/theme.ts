55e7ce016bfc4154c480a47674ae5196594536eb
Hussain-hamim <mohammadhussainafghan83@gmail.com>
2024-04-22 20:06:47 +0430

    implement dark mode.

---
+++ src/theme.ts
@@ -0,0 +1,8 @@
+import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+
+const config: ThemeConfig = {
+  initialColorMode: "dark",
+};
+
+const theme = extendTheme({ config });
+export default theme;
