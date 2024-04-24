bd300194341c8b98e3cf496eaa2592f36e7264bd
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-04-24 16:15:23 +0430

    adding documentation for the image optimization.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,10 @@
 const getCroppedImageUrl = (url: string) => {
   const target = "media/";
+  //get the index of the media/ keyword plus it's length to add the intended
+  //text after it to crop the image to 600/400. for image optimization.
   const index = url.indexOf(target) + target.length;
+  //return the url from start till media/ and add the below string and add
+  //the remaining url too from index till end to complete the url.
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 export default getCroppedImageUrl;
