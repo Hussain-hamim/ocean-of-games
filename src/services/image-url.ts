608e8505601b46d3c5affbc9c3957920126a49ab
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-04-25 22:03:02 +0430

    handle games with no image.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import hamimi from "../assets/hamimi.jpg";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return hamimi;
   const target = "media/";
   //get the index of the media/ keyword plus it's length to add the intended
   //text after it to crop the image to 600/400. for image optimization.
