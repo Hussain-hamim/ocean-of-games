f878cfe56a0da53ce953bd477a8a8fefb46ba85b
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-04-27 18:18:01 +0430

    clean ups...

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,11 +2,11 @@ import platforms from "../data/platforms";
 // import useData from "./useData";
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+// interface Platform {
+//   id: number;
+//   name: string;
+//   slug: string;
+// }
 
 // const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
 const usePlatforms = () => ({ data: platforms, error: null });
--- src/theme.ts
+++ src/theme.ts
@@ -1,5 +1,4 @@
 import { extendTheme, ThemeConfig } from "@chakra-ui/react";
-import { color } from "framer-motion";
 
 const config: ThemeConfig = {
   initialColorMode: "dark",
