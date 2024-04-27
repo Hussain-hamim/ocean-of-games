a8faad57cdf0fa798b7bd701496c151c4e57ba74
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-04-27 18:03:24 +0430

    customize the theme to get the darker grays.

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,14 @@
-import useData from "./useData";
+import platforms from "../data/platforms";
+// import useData from "./useData";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 interface Platform {
   id: number;
   name: string;
   slug: string;
 }
 
-const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+// const usePlatforms = () => useData<Platform>("/platforms/lists/parents");
+const usePlatforms = () => ({ data: platforms, error: null });
 
 export default usePlatforms;
