158ddecabd9d8d2b990ae7c0fdac817baae1e78e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-04 08:34:27 +0430

    install react-router for the project

--- src/components/ColorModeSwitch.tsx
+++ src/components/ColorModeSwitch.tsx
@@ -1,15 +1,10 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Text, useColorMode } from "@chakra-ui/react";
 
 const ColorModeSwitch = () => {
   const { toggleColorMode, colorMode } = useColorMode();
 
   return (
     <HStack>
-      <Switch
-        // colorScheme="green"
-        isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
-      />
       <Text onClick={toggleColorMode} as="button" whiteSpace="nowrap">
         {colorMode === "dark" ? "🔆" : "🌛"}
       </Text>
