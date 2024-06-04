193d0b0a0ac2d4c3f1947f507fb83120aa603098
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-04 14:26:49 +0430

    handle error with react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
-
 import "./index.css";
 import theme from "./theme.ts";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
