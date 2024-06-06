22e0f2dcb2962cfedc2af29fd3be225adea552cc
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-06 09:14:02 +0430

    ahhh error

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // root: "serve",
   plugins: [react()],
   build: {
     outDir: "dist",
-    rollupOptions: {
-      input: "serve/index.html",
-    },
+    // rollupOptions: {
+    //   input: "serve/index.html",
+    // },
   },
 });
