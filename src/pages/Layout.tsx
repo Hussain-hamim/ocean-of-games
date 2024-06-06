6e75bfa17de23ac982fbe0ea68254def7a4b959e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-06 11:33:26 +0430

    ...................................

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,14 @@
 import NavBar from "../components/NavBar";
 import { Box } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
+import HomePage from "./HomePage";
 
 const Layout = () => {
   return (
     <>
       <NavBar />
       <Box padding={5}>
+        <HomePage />
         <Outlet />
       </Box>
     </>
