a767fc97d01e3146eb986b15681ffe0193b0b0fc
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-06 11:49:47 +0430

    finally fixed it

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,16 +1,15 @@
 import NavBar from "../components/NavBar";
 import { Box } from "@chakra-ui/react";
-import { Outlet } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import HomePage from "./HomePage";
+import GameDetailPage from "./GameDetailPage";
 
 const Layout = () => {
+  const { slug } = useParams();
   return (
     <>
       <NavBar />
-      <Box padding={5}>
-        <HomePage />
-        <Outlet />
-      </Box>
+      <Box padding={5}>{slug ? <GameDetailPage /> : <HomePage />}</Box>
     </>
   );
 };
