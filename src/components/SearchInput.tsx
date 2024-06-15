4ef23a60d3d89f38f9147d84b51c7857c59fd048
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-15 17:22:03 +0430

    adding loading spinner for typing into search bar

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -3,8 +3,9 @@ import {
   InputGroup,
   InputLeftElement,
   InputRightElement,
+  Spinner,
 } from "@chakra-ui/react";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import useGameQueryStore from "../store";
 import { useNavigate } from "react-router-dom";
@@ -14,6 +15,7 @@ const SearchInput = () => {
   const ref = useRef<HTMLInputElement>(null);
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   const navigate = useNavigate();
+  const [suspense, setSuspense] = useState(false);
 
   return (
     <form
@@ -32,13 +34,23 @@ const SearchInput = () => {
           borderRadius={20}
           placeholder="Search games..."
           variant="filled"
+          onKeyDown={() => setSuspense(!suspense)}
         />
 
         <InputRightElement
           children={
             <span>
-              <Kbd>Ctrl</Kbd>
-              <Kbd marginRight={10}>K</Kbd>
+              {/* <Kbd>Ctrl</Kbd>
+              <Kbd marginRight={8}>K</Kbd> */}
+              {suspense && (
+                <Spinner
+                  thickness="2px"
+                  speed="0.65s"
+                  emptyColor="yellow.200"
+                  color="blue.500"
+                  // size="xl"
+                />
+              )}
             </span>
           }
         />
