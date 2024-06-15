86ba4664a363f5e116d9e0bc61bd599c74c308a8
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-06-15 17:07:33 +0430

    creating a branding heading

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,18 +1,30 @@
-import { HStack, Image } from "@chakra-ui/react";
+import { Box, HStack, Image } from "@chakra-ui/react";
 import water from "../assets/water.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
+import { Kbd } from "@chakra-ui/react";
 
 const NavBar = () => {
   return (
-    <HStack px="10px">
-      <Link to="/">
-        <Image borderRadius="50%" opacity={0.5} src={water} boxSize="60px" />
-      </Link>
-      <SearchInput />
-      <ColorModeSwitch />
-    </HStack>
+    <>
+      <Box textAlign="center" margin={2}>
+        <Kbd backgroundColor="blue.500" fontSize="2xl">
+          OCEAN
+        </Kbd>
+        <Kbd backgroundColor="grey">OF</Kbd>
+        <Kbd backgroundColor="#3a2339f0" fontSize="2xl">
+          GAMES
+        </Kbd>
+      </Box>
+      <HStack px="10px">
+        <Link to="/">
+          <Image borderRadius="50%" opacity={0.5} src={water} boxSize="60px" />
+        </Link>
+        <SearchInput />
+        <ColorModeSwitch />
+      </HStack>
+    </>
   );
 };
 
